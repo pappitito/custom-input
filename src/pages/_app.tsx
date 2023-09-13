018e59904235b295d41a66665c1acf9bf5846e21
Tito <107757681+pappitito@users.noreply.github.com>
2023-09-13 11:38:04 +0100

    input scaling fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ const montserrat = Montserrat({subsets: ['latin']})
 
 export default function App({ Component, pageProps }: AppProps) {
   return <main className={montserrat.className}>
-      <Component {...pageProps} />
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+           <Component {...pageProps} />
   </main>
 }
